a5978223f61cbf2c993a69b86d1445cd44a641e1
Lance Wilhelm <lance.wilhelm@gmail.com>
2021-05-01 22:04:26 -0700

    trying api

--- api/flaskUtils.py
+++ api/flaskUtils.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 
+from ledUtils import RotatingBlockGenerator
+
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=['GET'])
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
-@app.route("/api/temps", methods=['GET'])
-def getTemps():
+@app.route('/api/rotate', methods=['GET'])
+def runRotate():
+    rotating_block_generator = RotatingBlockGenerator()
+    if (not rotating_block_generator.process()):
+        rotating_block_generator.print_help()
     return
