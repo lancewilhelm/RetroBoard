84248ffe33ee64e107195abfa4345edb51fbe6fc
Lance Wilhelm <lance.wilhelm@gmail.com>
2021-05-01 22:07:45 -0700

    setup changes

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install python3-pip
-pip3 install flask flask-cors
+sudo pip3 install flask flask-cors
 curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/rgb-matrix.sh >rgb-matrix.sh
 sudo bash rgb-matrix.sh
