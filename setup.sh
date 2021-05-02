c70e4a17cbe81107cff55e2f6b77b4d7c28fc7b9
Lance Wilhelm <lance.wilhelm@gmail.com>
2021-05-01 21:57:30 -0700

    trying to get flask to work

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install python3-pip
+pip3 install flask flask-cors
 curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/rgb-matrix.sh >rgb-matrix.sh
 sudo bash rgb-matrix.sh
