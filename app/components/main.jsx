1a82c883e6c2ff3a316a8537c199f3879a3b1153
Lance Wilhelm <lance.wilhelm@gmail.com>
2021-05-02 09:05:46 -0700

    necessary api routing mod

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Main.module.css';
 
 export default function Main() {
     async function sendCommand(command) {
-        const url_base = 'http://127.0.0.1:5000/api/';
+        const url_base = 'http://0.0.0.0:5000/api/';
 
         const res = await fetch(url_base + command, {
             method: 'GET',
