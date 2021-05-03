1f17350fcdecf5bf4a250e75b6f07cfcc1f3ef73
Lance Wilhelm <lance.wilhelm@gmail.com>
2021-05-02 21:10:55 -0700

    docker privileged run on compose

--- setup.sh
+++ setup.sh
@@ -10,5 +10,7 @@ sudo pip3 install flask flask-cors
 wget http://ftp.us.debian.org/debian/pool/main/libs/libseccomp/libseccomp2_2.5.1-1_armhf.deb
 sudo dpkg -i libseccomp2_2.5.1-1_armhf.deb
 
+sudo pip3 -v install docker-compose
+
 curl https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/rgb-matrix.sh >rgb-matrix.sh
 sudo bash rgb-matrix.sh
