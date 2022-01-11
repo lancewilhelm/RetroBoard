e2a09c102d03bd9f877de05e1b3c71e278172382
lancewilhelm <lance.wilhelm@gmail.com>
2022-01-11 02:53:07 +0000

    ditched celery for threading. Get rid of this bran

--- api/routes.py
+++ api/routes.py
@@ -2,6 +2,7 @@
 from flask import render_template
 from utils import api
 import ledTasks
+import threading
 
 @api.route('/', methods=['GET'])
 def index_route():
@@ -10,8 +11,10 @@ def index_route():
 @api.route('/api/pixel', methods=['GET'])
 def pixel_route():
     print('starting route function')
+    test_thread = threading.Thread(target=ledTasks.pixel, daemon=True)
     try:
-        ledTasks.pixel()
+        test_thread.start()
+        print('hopefully this shows immediately and not after the animation is done')
     except Exception as e:
         print(e)
         
