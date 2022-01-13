64811cade7965197b52e9bd82041873b88d4d1e7
lancewilhelm <lance.wilhelm@gmail.com>
2022-01-12 21:21:12 -0800

    moved clock options to init, made font dict

--- api/ledTasks.py
+++ api/ledTasks.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import time
-from utils import matrix
+from utils import matrix, cent_x, cent_y, font_dict
 import time
 from PIL import Image, ImageDraw
 import logging
@@ -15,27 +15,23 @@ tasks = []
 #-------------------------------------------------------------------------
 # Utility functions:
 #-------------------------------------------------------------------------
-# The scale_color function is used to scale up any number into the 0 to 
-# 255 scale. This is useful for color calculations
 def scale_color(val, lo, hi):
+	'''Scales up any number into the 0 to 255 scale. This is useful for color calculations.'''
 	if val < lo:
 		return 0
 	if val > hi:
 		return 255
 	return 255 * (val - lo) / (hi - lo)
 
-# Rotation function used for the rotating block animation. Might be useful
-# elsewhere
 def rotate(x, y, sin, cos):
+	'''Used for the rotating block animation. Might be useful elsewhere'''
 	return x * cos - y * sin, x * sin + y * cos
 
 #-------------------------------------------------------------------------
 # Stoppable Thread Class:
-#  This is a class template that is used for making threads stoppable. We
-# will be using it to run led tasks on screen while we do other things
-# and then stop them when we request it
 #-------------------------------------------------------------------------
 class StoppableThread(threading.Thread):
+	''' Class used for making threads stoppable. Used to run led tasks on screen while other things run and then stop when requested'''
 	def __init__(self,  *args, **kwargs):
 		super(StoppableThread, self).__init__(*args, **kwargs)
 		self._stop_event = threading.Event()
@@ -53,105 +49,28 @@ class StoppableThread(threading.Thread):
 # wherever needed. This allows them to inherit the stoppable thread class
 # that is needed to run these animations asynchronously
 #-------------------------------------------------------------------------
-# A simple test animation for when we are getting started
-class TestAnimation(StoppableThread):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
 
-	def run(self):
-		logging.debug('starting test animation')
-		# RGB example w/graphics prims.
-		# Note, only "RGB" mode is supported currently.
-		image = Image.new("RGB", (32, 32))  # Can be larger than matrix if wanted!!
-		draw = ImageDraw.Draw(image)  # Declare Draw instance before prims
-		# Draw some shapes into image (no immediate effect on matrix)...
-		draw.rectangle((0, 0, 31, 31), fill=(0, 0, 0), outline=(0, 0, 255))
-		draw.line((0, 0, 31, 31), fill=(255, 0, 0))
-		draw.line((0, 31, 31, 0), fill=(0, 255, 0))
-
-		# Then scroll image across matrix...
-		for n in range(-32, 33):
-			if self.stopped():
-				break
-			matrix.Clear()
-			matrix.SetImage(image, n, n)
-			time.sleep(0.05)
-
-# This is a test function for scrolling text
-class RotatingBlock(StoppableThread):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-
-	def run(self):
-		logging.debug('starting rotating block')
-		cent_x = matrix.width / 2
-		cent_y = matrix.height / 2
-
-		rotate_square = min(matrix.width, matrix.height) * 1.41
-		min_rotate = cent_x - rotate_square / 2
-		max_rotate = cent_x + rotate_square / 2
-
-		display_square = min(matrix.width, matrix.height) * 0.7
-		min_display = cent_x - display_square / 2
-		max_display = cent_x + display_square / 2
-
-		deg_to_rad = 2 * 3.14159265 / 360
-		rotation = 0
-
-		# Pre calculate colors
-		col_table = []
-		for x in range(int(min_rotate), int(max_rotate)):
-			col_table.insert(x, scale_color(x, min_display, max_display))
-
-		offset_canvas = matrix.CreateFrameCanvas()
-
-		while True:
-			if self.stopped():
-				matrix.Clear()
-				return
-			rotation += 1
-			rotation %= 360
-
-			# calculate sin and cos once for each frame
-			angle = rotation * deg_to_rad
-			sin = math.sin(angle)
-			cos = math.cos(angle)
-
-			for x in range(int(min_rotate), int(max_rotate)):
-				for y in range(int(min_rotate), int(max_rotate)):
-					# Our rotate center is always offset by cent_x
-					rot_x, rot_y = rotate(x - cent_x, y - cent_x, sin, cos)
-
-					if x >= min_display and x < max_display and y >= min_display and y < max_display:
-						x_col = col_table[x]
-						y_col = col_table[y]
-						offset_canvas.SetPixel(rot_x + cent_x, rot_y + cent_y, 255, 0, 255)
-					else:
-						offset_canvas.SetPixel(rot_x + cent_x, rot_y + cent_y, 0, 0, 0)
-
-			offset_canvas = matrix.SwapOnVSync(offset_canvas)
-
-# Clock application
 class Clock(StoppableThread):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+		self.font = graphics.Font()
+		self.font_path = font_dict['4x6']
+		self.font.LoadFont(self.font_path)
+		self.font_height = self.font.height
+		self.font_width = self.font.CharacterWidth(ord('L'))
+		self.staticColor = graphics.Color(255, 255, 255)
+		self.position = {
+			'x': cent_x - (5 * self.font_width / 2),
+			'y': cent_y + (self.font_height / 2) - 2
+		}
 
 	def run(self):
 		logging.debug('starting clock')
 
+		# Establish the offscreen buffer to store changes too before publishing
 		offscreen_canvas = matrix.CreateFrameCanvas()
-		font = graphics.Font()
-		font.LoadFont("./fonts/7x13.bdf")
-		font_height = font.height
-		font_width = font.CharacterWidth(ord('L'))
-		textColor = graphics.Color(255, 255, 255)
-		cent_x = matrix.width / 2
-		cent_y = matrix.height / 2
-		pos = {
-			'x': cent_x - (5 * font_width / 2),
-			'y': cent_y + (font_height / 2) - 2
-		}
 
+		# Run the clock loop until stopped
 		while True:
 			# Check to see if we have stopped
 			if self.stopped():
@@ -186,7 +105,7 @@ class Clock(StoppableThread):
 				timeStr = hourStr + ':' + minStr
 			
 			# Write the actual drawing to the canvas and then display
-			graphics.DrawText(offscreen_canvas, font, pos['x'], pos['y'], textColor, timeStr)
+			graphics.DrawText(offscreen_canvas, self.font, self.position['x'], self.position['y'], self.staticColor, timeStr)
 			time.sleep(0.05)	# Time buffer added so as to not overload the system
 			offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
 
--- api/main.py
+++ api/main.py
@@ -7,10 +7,10 @@ import ledTasks
 
 # Start the flask server if this file was called as __main__
 if __name__ == '__main__':
-	logging.debug('Starting the clock as an opening app')
+	logging.debug('starting the clock as an opening app')
 	clock = ledTasks.Clock()
 	ledTasks.tasks.append(clock)
 	clock.start()
 	
-	logging.info('starting the flask server')
+	logging.info('STARTING THE WARP ENGINES (starting flask)')
 	api.run(host='0.0.0.0')
