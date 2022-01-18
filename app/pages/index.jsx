870165d9a965f92d2df7d69a374e1266e13e0d52
Lance Wilhelm <lance.wilhelm@gmail.com>
2022-01-17 21:32:16 -0800

    added reset functionality

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -6,31 +6,38 @@ import { Gear } from 'react-bootstrap-icons';
 import { useState } from 'react';
 import SettingsModal from '../components/SettingsModal';
 
-
 export default function Home() {
-	// Set the state variable for the modal
-	const [modalOpen, setModalOpen] = useState(false);
+    // Set the state variable for the modal
+    const [modalOpen, setModalOpen] = useState(false);
 
-	const handleModalOpen = () => setModalOpen(true);
-	const handleModalClose = () => setModalOpen(false);
+    const handleModalOpen = () => setModalOpen(true);
+    const handleModalClose = () => setModalOpen(false);
 
-	return (
-		<div className={styles.container}>
-			<Head>
-				<title>Retro Magic Board</title>
-			</Head>
-			<div className={styles.menuBar}>
-			<Button className={styles.settingsButton} variant='outline-dark' onClick={handleModalOpen}>
-				<Gear />
-			</Button>
-			</div>
-			<div className={styles.titleBar}>
-				<h1 className={styles.title}>Magic Color Board</h1>
-				<div className={styles.subtitle}>A board of wonders</div>
-				<Main className={styles.main}/>
-			</div>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Retro Magic Board</title>
+            </Head>
+            <div className={styles.menuBar}>
+                <Button
+                    className={styles.settingsButton}
+                    variant='outline-dark'
+                    onClick={handleModalOpen}
+                >
+                    <Gear />
+                </Button>
+            </div>
+            <div className={styles.titleBar}>
+                <h1 className={styles.title}>Magic Color Board</h1>
+                <div className={styles.subtitle}>A board of wonders</div>
+                <Main className={styles.main} />
+            </div>
 
-			<SettingsModal modalOpen={modalOpen} handleModalOpen={handleModalOpen} handleModalClose={handleModalClose}/>
-		</div>
-	);
+            <SettingsModal
+                modalOpen={modalOpen}
+                handleModalOpen={handleModalOpen}
+                handleModalClose={handleModalClose}
+            />
+        </div>
+    );
 }
