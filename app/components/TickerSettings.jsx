0dd0d6412335372abb4c8582c167e32c2491c9bc
Lance Wilhelm <lance.wilhelm@gmail.com>
2022-02-06 18:58:45 -0800

    added the basic ticker settings

--- app/components/TickerSettings.jsx
+++ app/components/TickerSettings.jsx
@@ -1,37 +1,97 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import styles from '../styles/TickerSettings.module.css';
-import { Dialog, DialogContent, DialogTitle, DialogActions, Button, Autocomplete, Chip, TextField } from '@mui/material';
+import { Dialog, DialogContent, DialogTitle, DialogActions, Button, Autocomplete, Chip, TextField, getRadioUtilityClass } from '@mui/material';
 import { localIP } from './config';
 
 export default function TickerSettings(props) {
 	const [settings, setSettings] = useState({});
 	const [scroll, setScroll] = useState('paper');
-	const [tickerType, setTickerType] = useState();
+	const [equityType, setEquityType] = useState();
+	const [graphType, setGraphType] = useState();
+	const [symbol, setSymbol] = useState();
+	const [symbolList, setSymbolList] = useState([]);
+	const [displaySymbolList, setDisplaySymbolList] = useState([]);
 
-	const typeList = ['stock', 'crypto'];
+	const equityTypeList = ['stock', 'crypto'];
+	const graphTypeList = ['filled', 'bar'];
 
-	function changeTickerType(e, val) {
-		setTickerType(val);
+	const apiKeys = require('../../apikeys.json');
+
+	function getSymbolList(type) {
+        if (equityType == 'crypto') {
+            fetch(
+                'https://finnhub.io/api/v1/crypto/symbol?exchange=binance&token=' +
+                    apiKeys.finnhub
+            )
+                .then((res) => res.json())
+                .then((data) => {
+					const display_symbol_list = data.map(a => a.displaySymbol);
+					const symbol_list = data.map(a => a.symbol);
+                    setSymbolList(symbol_list);
+					setDisplaySymbolList(display_symbol_list);
+                });
+        } else {
+			fetch(
+                'https://finnhub.io/api/v1/stock/symbol?exchange=US&token=' +
+                    apiKeys.finnhub
+            )
+                .then((res) => res.json())
+                .then((data) => {
+					const display_symbol_list = data.map(a => a.displaySymbol);
+					const symbol_list = data.map(a => a.symbol);
+                    setSymbolList(symbol_list);
+					setDisplaySymbolList(display_symbol_list);
+                });
+		}
+    }
+
+	function changeEquityType(e, val) {
+		setEquityType(val);
+		let settings_copy = Object.assign({}, props.settings);
+		settings_copy.ticker.equity_type = val;
+		props.setSettings(settings_copy);
+	}
+
+	function changeGraphType(e, val) {
+		setGraphType(val);
+		let settings_copy = Object.assign({}, props.settings);
+		settings_copy.ticker.graph_type = val;
+		props.setSettings(settings_copy);
+	}
+
+	function changeSymbol(e, val) {
+		const s = symbolList[displaySymbolList.indexOf(val)]
+		setSymbol(s);
+		let settings_copy = Object.assign({}, props.settings);
+		settings_copy.ticker.symbol = s;
+		props.setSettings(settings_copy);
 	}
 
 	function sendSettings() {
 		const requestOptions = {
 			method: 'POST',
 			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(settings),
+			body: JSON.stringify(props.settings),
 		};
 		fetch('http://' + localIP + ':5000/api/settings', requestOptions);
-		props.handleDialogClose();
+		props.handleTickerSettingsClose();
 	}
 
 	useEffect(() => {
-        fetch('http://' + localIP + ':5000/api/settings')
-            .then((res) => res.json())
-            .then((data) => {
-                props.setSettings(data);
-            });
-    }, [props.resetSettings]);
+		fetch('http://' + localIP + ':5000/api/settings')
+			.then((res) => res.json())
+			.then((data) => {
+				props.setSettings(data);
+				setEquityType(data.ticker.equity_type);
+				setGraphType(data.ticker.graph_type);
+				setSymbol(data.ticker.symbol);
+			});
+	}, [props.resetSettings]);
+
+	useEffect(() => {
+		getSymbolList();
+	}, [props.tickerSettingsOpen, equityType])
 
 	return (
 		<div className={styles.container}>
@@ -39,6 +99,7 @@ export default function TickerSettings(props) {
 				open={props.tickerSettingsOpen}
 				onClose={props.handleTickerSettingsClose}
 				scroll={scroll}
+				fullScreen={true}
 				aria-labelledby="scroll-dialog-title"
 				aria-describedby="scroll-dialog-description"
 				className={styles.dialog}
@@ -50,14 +111,42 @@ export default function TickerSettings(props) {
 							disablePortal
 							className={styles.textBox}
 							id='color-mode-selector'
-							options={typeList}
+							options={equityTypeList}
 							sx={{ width: 200 }}
-							onChange={(e, val) => changeTickerType(e, val)}
+							onChange={(e, val) => changeEquityType(e, val)}
 							renderInput={(params) => (
 								<TextField {...params} label='Type' />
 							)}
 						/>
-						<Chip label={tickerType} variant='outlined' />
+						<Chip label={equityType} variant='outlined' />
+					</div>
+					<div className={styles.settingsContainer}>
+						<Autocomplete
+							disablePortal
+							className={styles.textBox}
+							id='color-mode-selector'
+							options={graphTypeList}
+							sx={{ width: 200 }}
+							onChange={(e, val) => changeGraphType(e, val)}
+							renderInput={(params) => (
+								<TextField {...params} label='Type' />
+							)}
+						/>
+						<Chip label={graphType} variant='outlined' />
+					</div>
+					<div className={styles.settingsContainer}>
+						<Autocomplete
+							disablePortal
+							className={styles.textBox}
+							id='color-mode-selector'
+							options={displaySymbolList}
+							sx={{ width: 200 }}
+							onChange={(e, val) => changeSymbol(e, val)}
+							renderInput={(params) => (
+								<TextField {...params} label='Type' />
+							)}
+						/>
+						<Chip label={symbol} variant='outlined' />
 					</div>
 				</DialogContent>
 				<DialogActions>
@@ -80,7 +169,7 @@ export default function TickerSettings(props) {
 					className={styles.button}
 					variant='outlined' 
 					color='success'
-					onClick={null}>
+					onClick={sendSettings}>
 						Save Changes
 					</Button>
 				</DialogActions>
